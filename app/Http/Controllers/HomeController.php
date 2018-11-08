c9db67ddebff4749dffc35fcc149424645d8ebd5
Max Alekhnovich <mla23@njit.edu>
2018-11-07 20:05:24 -0500

    fixing doc comment

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,8 +2,26 @@
 
 
 /**
- * this is the home controller
+ * Short description for file
+ *
+ * Long description for file (if any)...
+ *
+ * PHP version 5
+ *
+ * LICENSE: This source file is subject to version 3.01 of the PHP license
+ * that is available through the world-wide-web at the following URI:
+ * http://www.php.net/license/3_01.txt.  If you did not receive a copy of
+ * the PHP License and are unable to obtain it through the web, please
+ * send a note to license@php.net so we can mail you a copy immediately.
+ *
+ * @category   CategoryName
+ * @package    PackageName
+ * @author     Original Author <author@example.com>
  */
+
+
+
+
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
