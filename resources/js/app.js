82aeb879b9c5fe7f50044fa86949342458a97866
lukaszvikas <lzvikas1@gmail.com>
2018-11-13 14:21:23 -0500

    Task: added the navigation bar

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,22 +1,14 @@
+require("./bootstrap");
 
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
+import Vue from "vue";
+import Vuetify from "vuetify";
+import App from "./App.vue";
+import "vuetify/dist/vuetify.min.css";
 
-require('./bootstrap');
+//Vue.config.productionTip = false;
 
-window.Vue = require('vue');
+Vue.use(Vuetify);
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
-
-const app = new Vue({
-    el: '#app'
-});
+new Vue({
+    render: h => h(App)
+}).$mount("#app")
---
+++ resources/js/components/Navigation.vue
@@ -0,0 +1,65 @@
+<template>
+    <div>
+        <v-navigation-drawer
+                v-model="drawer"
+                fixed
+                clipped
+                class="grey lighten-4"
+                app
+        >
+            <v-list
+                    dense
+                    class="grey lighten-4"
+            >
+                <template v-for="(item, i) in navItems">
+                    <v-divider
+                            v-if="item.divider"
+                            :key="i"
+                            dark
+                            class="my-3"
+                    ></v-divider>
+                    <v-list-tile
+                            v-else
+                            :key="i"
+                    >
+                        <v-list-tile-action>
+                            <v-icon>{{ item.icon }}</v-icon>
+                        </v-list-tile-action>
+                        <v-list-tile-content>
+                            <v-list-tile-title class="grey--text">
+                                {{ item.text }}
+                            </v-list-tile-title>
+                        </v-list-tile-content>
+                    </v-list-tile>
+                </template>
+            </v-list>
+        </v-navigation-drawer>
+        <v-toolbar color="amber" app absolute clipped-left>
+            <v-toolbar-side-icon @click.native="drawer = !drawer"></v-toolbar-side-icon>
+            <span class="title ml-3 mr-5">FAQBot</span>
+            <v-text-field
+                    solo-inverted
+                    flat
+                    hide-details
+                    label="Search"
+                    prepend-inner-icon="search"
+            ></v-text-field>
+            <v-spacer></v-spacer>
+        </v-toolbar>
+    </div>
+</template>
+
+<script>
+    export default {
+        data: () => ({
+            drawer: null,
+            navItems: [
+                { icon: "home", text: "Home", href: "/" },
+                { divider: true },
+                { icon: "question_answer", text: "NJIT FAQ Bot", href: "/" },
+                { icon: "info", text: "About", href: "/" }
+            ]
+        })
+    };
+</script>
+
